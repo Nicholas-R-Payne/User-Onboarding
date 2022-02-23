0637d2d603fee92f477849c6efd6189acedeccd6
DarkedgeTH <darkedgeth@gmail.com>
2022-02-23 14:49:36 -0500

    imported yup and added validate function to App.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 
 import schema from './Validation/formSchema'
-
+import * as yup from 'yup'
 import Form from './Components/Form'
 
 const initialFormValues = {
@@ -19,8 +19,15 @@ function App() {
     
   }
 
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
   const handleChange = (name, value) => {
-    setFormValues({...formValues, [name]: value});
+    setFormValues({ ...formValues, [name]: value });
   }
 
   return (
