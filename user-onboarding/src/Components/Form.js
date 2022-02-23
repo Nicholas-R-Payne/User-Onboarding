9abc7d12c1839d4a7b905fe454c6d476a6a613f4
DarkedgeTH <darkedgeth@gmail.com>
2022-02-23 14:15:07 -0500

    set up structure for Form

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -2,7 +2,44 @@ import React from 'react'
 
 const Form = (props) => {
     return (
-        <h1>Onboarding Form</h1>
+        <div>
+            <h1>Onboarding Form</h1>
+            <form>
+                <label>Name:
+                    <input
+                        type='text'
+                        name='username'
+                        value={username}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Email:
+                    <input
+                        type='email'
+                        name='email'
+                        value={email}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Password:
+                    <input
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Terms of Service:
+                    <input
+                        type='checkbox'
+                        name='tos'
+                        checked={checked}
+                        onChange={onChange}
+                    />
+                </label>
+                <input type='submit' value='Create a Friend' />
+            </form>
+        </div>
     )
 }
 
