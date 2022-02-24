33d66078316eb0cb2f7d372507e57cf96e05ab79
DarkedgeTH <darkedgeth@gmail.com>
2022-02-24 15:31:42 -0500

    seperated tests into multiple tests

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -7,21 +7,26 @@ describe('Form App', () => {
     const emailInput = () => cy.get('input[name=email]') 
     const passwordInput = () => cy.get('input[name=password]')
     const tosCheckbox = () => cy.get('input[name=tos]')
-    console.log(tosCheckbox)
 
-    it('can type in the inputs', () => {
+    it('can type in username input', () => {
         usernameInput()
             .should('have.value', '')
             .type('TomRiddle')
             .should('have.value', 'TomRiddle')
+    })
+    it('can type in email input', () => {
         emailInput()
             .should('have.value', '')
             .type('lordvoldemort1@gmail.com')
             .should('have.value', 'lordvoldemort1@gmail.com')
+    })
+    it('can type in password input', () => {
         passwordInput()
             .should('have.value', '')
             .type('snapekilleddumbledore')
             .should('have.value', 'snapekilleddumbledore')
+    })
+    it('can check tos checkbox', () => {
         tosCheckbox()
             .should('not.be.checked')
             .check()
